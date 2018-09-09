1291c82b715b62789fac1578c33893fdf73ad5de
Alex Zhou <alexzhou@dhcp-rhodes-1417.eduroam.cornell.edu>
2018-09-09 14:16:32 -0400

    working condractions and changed to include , and --

--- training.py
+++ training.py
@@ -2,42 +2,48 @@ import nltk
 nltk.download('punkt')
 
 def getSpeeches():
-	f=open('Assignment1_resources/train/obama.txt','rt')
-	raw = f.read()
-	obamaTokens = nltk.word_tokenize(raw)
-	obamaTokens = cleanText(obamaTokens)
-	f.close()
+    f=open('Assignment1_resources/train/obama.txt','rt')
+    raw = f.read()
+    obamaTokens = nltk.word_tokenize(raw)
+    obamaTokens = cleanText(obamaTokens)
+    f.close()
 
-	f=open('Assignment1_resources/train/trump.txt','rt')
-	raw = f.read()
-	trumpTokens = nltk.word_tokenize(raw)
-	trumpTokens = cleanText(trumpTokens)
-	f.close()
+    f=open('Assignment1_resources/train/trump.txt','rt')
+    raw = f.read()
+    trumpTokens = nltk.word_tokenize(raw)
+    trumpTokens = cleanText(trumpTokens)
+    f.close()
 
-	return (obamaTokens,trumpTokens);
-
+    return (obamaTokens,trumpTokens);
 
 def cleanText(tokens):
-	#remove certain puncuations 
-	unwanted = ["--", ",", "``", "“", "”"]
-	tokens = [word for word in tokens if word not in unwanted]
+    #remove certain puncuations 
+    #unwanted = ["--", ",", "``", "“", "”"] 
+    unwanted = ["``", "“", "”"] #with commans and dashes
+    tokens = [word for word in tokens if word not in unwanted]
 
-	#combine conjoined words
-	result = []
-	for i in range(len(tokens)-1):
-		if(tokens[i+1] == "’"):
-			v = "" + tokens[i]+tokens[i+1]+tokens[i+2]
-			result.append(v)
-			i = i + 2
-		else:
-			result.append(tokens[i])
-	
-	return result
+    #combine conjoined words
+    i = 0
+    result = []
+    while i < len(tokens)-1:
+        if(tokens[i+1] == "’"):
+            v = "" + tokens[i]+tokens[i+1]+tokens[i+2]
+            result.append(v)
+            i += 3
+        elif(tokens[i+1] == "'s"):
+            v = "" + tokens[i] + tokens[i+1]
+            result.append(v)
+            i += 2
+        else:
+            result.append(tokens[i])
+            i += 1
+
+    return result
 
 def main():
-	(obama,trump) = getSpeeches();
-	print(trump[:500])
+    (obama,trump) = getSpeeches();
+    print(trump[:600])
 
 
 if __name__ == "__main__":
-	main()
+    main()
