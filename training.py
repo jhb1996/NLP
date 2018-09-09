276018b7d344fad7691514e47c92d05515c0642b
Alex Zhou <alexzhou@dhcp-rhodes-1417.eduroam.cornell.edu>
2018-09-09 15:49:56 -0400

    change to list of list

--- training.py
+++ training.py
@@ -4,28 +4,36 @@ nltk.download('punkt')
 def getSpeeches():
     f=open('Assignment1_resources/train/obama.txt','rt')
     raw = f.read()
-    obamaTokens = nltk.word_tokenize(raw)
-    obamaTokens = cleanText(obamaTokens)
+    split = raw.split("\n")
+    obama = []
+    for paragraph in split:
+        obamaTokens = nltk.word_tokenize(paragraph)
+        obamaTokens = cleanText(obamaTokens)
+        obama.append(obamaTokens)
     f.close()
 
     f=open('Assignment1_resources/train/trump.txt','rt')
     raw = f.read()
-    trumpTokens = nltk.word_tokenize(raw)
-    trumpTokens = cleanText(trumpTokens)
+    split = raw.split("\n")
+    trump = []
+    for paragraph in split:
+        trumpTokens = nltk.word_tokenize(paragraph)
+        trumpTokens = cleanText(trumpTokens)
+        trump.append(trumpTokens)
     f.close()
 
-    return (obamaTokens,trumpTokens);
+    return (obama,trump);
 
 def cleanText(tokens):
     #remove certain puncuations 
     #unwanted = ["--", ",", "``", "“", "”"] 
-    unwanted = ["``", "“", "”"] #with commans and dashes
+    unwanted = ["``", "“", "”"] #with commas and dashes
     tokens = [word for word in tokens if word not in unwanted]
 
     #combine conjoined words
     i = 0
     result = []
-    while i < len(tokens)-1:
+    while i < len(tokens)-3:
         if(tokens[i+1] == "’"):
             v = "" + tokens[i]+tokens[i+1]+tokens[i+2]
             result.append(v)
@@ -38,11 +46,12 @@ def cleanText(tokens):
             result.append(tokens[i])
             i += 1
 
+    result += tokens[-3:]
     return result
 
 def main():
     (obama,trump) = getSpeeches();
-    print(trump[:600])
+    print(trump[2])
 
 
 if __name__ == "__main__":
